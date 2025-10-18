8b0356b2270f845fab3ccd51b730b7f3ebc7263c
Mohammed-Saleh-Ishaq <mohammedishaq0592@gmail.com>
2025-10-19 04:46:09 +0530

    update main.py

--- app/main.py
+++ app/main.py
@@ -120,6 +120,11 @@ async def command(cmd: str = Form(...), filename: str = Form(...)):
     return {"action": "none", "message": "Coming soon! "}
 
 # Run the app with Uvicorn (for development)
+#i#f __name__ == "__main__":
+##    import uvicorn
+##    uvicorn.run(app, host="0.0.0.0", port=8000)
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
